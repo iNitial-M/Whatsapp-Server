5893ed89bbf123e762112ee6468d1957866ce062
M Lukman <initial.m503@gmail.com>
2022-07-14 13:35:36 +0700

    Fix Bug

---
+++ src/api/class/session.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unsafe-optional-chaining */
+const { WhatsAppInstance } = require('../class/instance')
+const logger = require('pino')()
+
+class Session {
+    async restoreSessions() {
+        let restoredSessions = new Array()
+        let allCollections = []
+        try {
+            const db = mongoClient.db('Devices_ID')
+            const result = await db.listCollections().toArray()
+            result.forEach((collection) => {
+                allCollections.push(collection.name)
+            })
+
+            allCollections.map((key) => {
+                const query = {}
+                db.collection(key)
+                    .find(query)
+                    .toArray(async (err, result) => {
+                        if (err) throw err
+                        const instance = new WhatsAppInstance(key)
+                        await instance.init()
+                        WhatsAppInstances[key] = instance
+                    })
+                restoredSessions.push(key)
+            })
+        } catch (e) {
+            logger.error('Error restoring sessions')
+            logger.error(e)
+        }
+        return restoredSessions
+    }
+}
+
+exports.Session = Session
